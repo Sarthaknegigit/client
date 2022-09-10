884d619f94b8c111a848b1836730bbfc6216d1d7
SarthakMac <sarthakmac@SarthakMacs-MacBook-Pro.local>
2022-09-10 14:48:55 +0530

    Style changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,17 @@
-import './header.css'
+import "./header.css";
 
 export default function Header() {
   return (
     <div className="header">
-        <div className="headerTitles">
-            <span className="headerTitleSm">React & Node</span>
-            <span className="headerTitleLg">Blog</span>
-        </div>
-        <img 
-        src='https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+      <div className="headerTitles">
+        <span className="headerTitleSm">Code and Community</span>
+        <span className="headerTitleLg">Decypher</span>
+      </div>
+      <img
+        src="https://images8.alphacoders.com/617/617304.jpg"
         alt="headerImg"
-        className="headerImg" />
+        className="headerImg"
+      />
     </div>
-  )
+  );
 }
